f20271d31884ce0de360d6c51c23688bdfd71142
dustinpezley <dustinpezley@gmail.com>
2022-07-20 23:22:09 -0500

    added questions, connection, server, and began routes files

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+//Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '1610GanyM3d3',
+    database: 'organization'
+  }, 
+);
+
+module.exports = db;
