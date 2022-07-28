ec1de8782433ac2c06b59465b34084523bf75d21
dustinpezley <dustinpezley@gmail.com>
2022-07-27 22:24:41 -0500

    updated employee db calls to correctly function and improve output tables

--- app.js
+++ app.js
@@ -1,79 +1,80 @@
+//=====Dependencies=====//
 const db = require('./db/connection');
-const prompts = require('./lib/questions');
+const questions = require('./lib/questions');
 const { showDepartments, addDepartment, deleteDepartment, showDepartmentEmployees, showDepartmentBudgets } = require('./lib/department');
 const { showRoles, addRole, deleteRole } = require('./lib/role');
 const { showEmployees, addEmployee, updateEmployeeRole, updateEmployeeManager, deleteEmployee } = require('./lib/employee');
 
 db.connect(err => {
   if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}.`);
-  });
-
   // Log logo into terminal
   console.log("+-------------------------------------------------------+");
   console.log("|                                                       |");
   console.log("|     _____                 _                           |");
   console.log("|    |  ___|_ __ ___  _ __ | | ___  _   _  ___  ___     |");
-  console.log("|    |  _| | '_ ` _ \| '_ \| |/ _ \| | | |/ _ \/ _ \    |");
+  console.log("|    |  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\    |");
   console.log("|    | |___| | | | | | |_) | | (_) | |_| |  __/  __/    |");
-  console.log("|    |_____|_| |_| |_| .__/|_|\___/\___, |\___|\___|    |");
+  console.log("|    |_____|_| |_| |_| .__/|_|\\___/\\___, |\\___|\\___|    |");
   console.log("|                    |_|            |___/               |");
   console.log("|     __  __                                            |");
-  console.log("|    |  \/  | __ _ _ __   __ _  __ _  ___ _ __          |");
-  console.log("|    | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|         |");
+  console.log("|    |  \\/  | __ _ _ __   __ _  __ _  ___ _ __          |");
+  console.log("|    | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__|         |");
   console.log("|    | |  | | (_| | | | | (_| | (_| |  __/ |            |");
-  console.log("|    |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|            |");
+  console.log("|    |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|            |");
   console.log("|                               |__/                    |");
   console.log("|                                                       |");
   console.log("+-------------------------------------------------------+");
 
-  //Call questions
-  prompts()
-    .then((answers) => {
-      const { initialChoices } = answers;
+  // Confirm database connection
+  console.log('Database connected.');
 
-      switch (initialChoices) {
-        case 'VIEW all departments':
-          showDepartments();
-          break;
-        case 'VIEW all roles':
-          showRoles();
-          break;
-        case 'VIEW all employess':
-          showEmployees();
-          break;
-        case 'VIEW employees by department':
-          showDepartmentEmployees();
-          break;
-        case 'VIEW department budgets':
-          showDepartmentBudgets();
-          break;
-        case 'ADD a department':
-          addDepartment(answers.department);
-          break;
-        case 'ADD a role':
-          addRole(answers.roleName, answers.roleSalary);
-          break;
-        case 'ADD an employee':
-          addEmployee(answers.employeeFirstName, answers.employeeLastName);
-          break;
-        case 'UPDATE an employee role':
-          updateEmployeeRole();
-          break;
-        case "UPDATE an employee's manager":
-          updateEmployeeManager();
-          break;
-        case 'DELETE a department':
-          deleteDepartment();
-          break;
-        case 'DELETE a role':
-          deleteRole();
-          break;
-        case 'DELETE an employee':
-          deleteEmployee();
-          break;
-      }
-    });
+  //Call questions
+  questions()
+  .then(answers => {
+    switch (answers.initialChoices) {
+      case 'VIEW all departments':
+        showDepartments();
+        break;
+      case 'VIEW all roles':
+        showRoles();
+        break;
+      case 'VIEW all employess':
+        showEmployees();
+        break;
+      case 'VIEW employees by department':
+        showDepartmentEmployees();
+        break;
+      case 'VIEW department budgets':
+        showDepartmentBudgets();
+        break;
+      case 'ADD a department':
+        addDepartment(answers.department);
+        break;
+      case 'ADD a role':
+        addRole(answers.roleName, answers.roleSalary);
+        break;
+      case 'ADD an employee':
+        addEmployee(answers.employeeFirstName, answers.employeeLastName);
+        break;
+      case 'UPDATE an employee role':
+        updateEmployeeRole();
+        break;
+      case "UPDATE an employee's manager":
+        updateEmployeeManager();
+        break;
+      case 'DELETE a department':
+        deleteDepartment();
+        break;
+      case 'DELETE a role':
+        deleteRole();
+        break;
+      case 'DELETE an employee':
+        deleteEmployee();
+        break;
+      case 'QUIT session':
+        db.end();
+        break;
+      default: db.end();
+    }
+  });
 });
